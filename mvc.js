07eccb9992d1d5b9c1f30f04aa46f39f61785530
Charles <whitfield.cw@gmail.com>
2015-11-24 01:12:22 -0800

    add ajax to load app listings

--- mvc.js
+++ mvc.js
@@ -1,31 +1,69 @@
 //headers with route options
 //body showing results as table data
 
+var request = {
+  apiUrl: "http://localhost:3000/rankings/20150508/apps?",
+  data: null,
+  reqHeader: function(xhr){
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("Access-Control-Allow-Origin", "http://localhost:8000");
+  },
+  getData: function(query){
+    var opts = {method: "GET", url: (request.apiUrl)+query, config: request.reqHeader};
+    m.request(opts).then(function(pageData){
+      request.data = pageData
+      // m.redraw();
+    })
+  }
+}
+
 var Menu = {
   view: function(){
     return[
-      btn("Paid", "/paid"),
-      btn("Free", "/free"),
-      btn("Top Grossing", "/top-grossing")
+      btn("Paid", "/paid", "type=paid&limit=40&sort=downloads"),
+      btn("Free", "/free", "type=free&limit=40&sort=downloads"),
+      btn("Top Grossing", "/top-grossing", "type=all&limit=40&sort=revenue")
     ];
 
-    function btn(name, route){
+    function btn(name, route, query){
       var isCurrent = (m.route() === route);
-      var click = function(){ m.route(route); };
+      var click = function(){
+        request.getData(query)
+
+        // (pageObj[route]).controller.getData()
+        m.route(route);
+      };
       return m("button"+(isCurrent ? ".success" : ""), {onclick: click}, name);
     }
   }
 };
 
-function Page(content){
+function PageView(){
   this.view = function(){
-    return [ Menu.view(), m("hr"), m("p", content)]
+    var data = request.data
+    if (data){
+      console.log(data)
+      return [ Menu.view(), m("hr"), m("div", data.map(function(app){
+        return m("h3", app.name);
+      }))]
+    } else {
+      return [Menu.view(), m("hr"), m("h4", "Problem retrieving data")]
+    }
   }
 }
 
-var Paid = new Page("Most downloaded paid apps")
-var Free = new Page("Most downloaded free apps")
-var TopGrossing = new Page("Top-grossing apps")
+// var Paid = new PageView("/paid", "type=paid&limit=40&sort=downloads")
+// var Free = new PageView("/free", "type=free&limit=40&sort=downloads")
+// var TopGrossing = new PageView("/top-grossing", "type=all&limit=40&sort=revenue")
+// var pageObj = {
+//   "/paid": Paid,
+//   "/free": Free,
+//   "/top-grossing": TopGrossing
+// }
+
+var Paid = new PageView()
+var Free = new PageView()
+var TopGrossing = new PageView()
 
 m.route(document.getElementById("divvy"), "/paid", {
   "/paid": Paid,
